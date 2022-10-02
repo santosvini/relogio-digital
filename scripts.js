ad334eeb38bebc99c61844696a586f6efa5c2274
Vini Santos <vini_santos7@live.com>
2022-10-02 00:24:13 -0300

    :art: corrigindo código e craindo readme

--- scripts.js
+++ scripts.js
@@ -8,9 +8,9 @@ const relogio = setInterval(function time() {
   let min = dateToday.getMinutes();
   let sec = dateToday.getSeconds();
 
-  if (hour < 10) hr = '0' + hour;
-  if (min < 10) hr = '0' + min;
-  if (sec < 10) hr = '0' + sec ;
+  if (hour < 10) hour = '0' + hour;
+  if (min < 10) min = '0' + min;
+  if (sec < 10) sec = '0' + sec ;
 
   horas.textContent = hour;
   minutos.textContent = min;
